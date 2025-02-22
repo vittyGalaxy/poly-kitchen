b9a5bf7266b9d69500b9d2bed5b0f12140e4c6d1
Vittorione <vittorio.tiozzo.dev@gmail.com>
2025-02-22 16:59:58 +0100

    test: update README

--- README.md
+++ README.md
@@ -20,3 +20,15 @@ poly-kitchen
                 └── Pizza.java
 
 ```
+
+## Output
+```
+    Sto preparando Patapizza
+    Sto preparando Patapizza e ci mettero' 30 minuti
+    Sto preparando Cioccolato
+    Sto preparando Cioccolato e ci mettero' 2 minuti
+    Sto preparando Setteveli
+    Sto preparando Setteveli e ci mettero' 20 minuti
+    Sto preparando Carbonara
+    Sto preparando Carbonara e ci mettero' 10 minuti
+```
--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,21 @@ import vitty.oop.kitchen.*;
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cook cook = new Cook("Carlo Cracco", 2);
+        Dish dish = new Pizza("Patapizza");
+        cook.prepareDish(dish);
+        cook.prepareDish(dish, 30);
+
+        Dish gelato = new IceCream("Cioccolato", 1);
+        cook.prepareDish(gelato);
+        cook.prepareDish(gelato, 2);
+
+        Dish cake = new Cake("Setteveli");
+        cook.prepareDish(cake);
+        cook.prepareDish(cake, 20);
+
+        Dish pasta = new Pasta("Carbonara");
+        cook.prepareDish(pasta);
+        cook.prepareDish(pasta, 10);
     }
 }
