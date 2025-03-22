21c43dc436ac93838b5a5b747e593d3d27c7d065
Vittorione <vittorio.tiozzo.dev@gmail.com>
2025-03-22 22:13:45 +0100

    feat: added Ingredient array

--- src/vitty/oop/kitchen/Dish.java
+++ src/vitty/oop/kitchen/Dish.java
@@ -2,6 +2,8 @@ package vitty.oop.kitchen;
 
 public class Dish {
     protected String name;
+    protected int nIngredient;
+    protected Ingredient[] ingredient = new Ingredient[nIngredient];
 
     // Builder
     public Dish(String name) {
@@ -13,13 +15,26 @@ public class Dish {
         return name;
     }
 
+    public int getnIngredient() {
+        return nIngredient;
+    }
+
     // Setter
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setnIngredient(int nIngredient) {
+        this.nIngredient = nIngredient;
     }
 
     // Methods
     public void prepare(){}
 
     public void prepare(int time){}
+
+    public void addIngredient(Ingredient ingredient){
+        nIngredient++;
+        this.ingredient[nIngredient] = ingredient;
+    }
 }
